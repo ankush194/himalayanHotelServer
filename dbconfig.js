40b83cda1d32edb5f540e3c14ff8017d403cc1e2
rohit-web-tech <rohitdogra0126@gmail.com>
2023-11-24 04:27:38 +0530

    third

--- dbconfig.js
+++ dbconfig.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose") ;
 
 const db_url = process.env.db_url ;
 
-mongoose.connect(db_url);
+mongoose.connect(process.env.database, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
 
 const con = mongoose.connection ;
 
